653d0b1d4d46cf01c66f77ec127ea232fac912f9
AlexRoss <alexeyrossoshw@gmail.com>
2018-07-24 13:50:02 +0300

    project initialization

---
+++ Vigor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Vigor
+//
+//  Created by Алексей Россошанский on 24.07.2018.
+//  Copyright © 2018 Rentateam. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

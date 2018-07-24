b88330115a296c9fe202f961e92b19bcd931b58d
AlexRoss <alexeyrossoshw@gmail.com>
2018-07-24 15:48:11 +0300

    VIG 1-3

--- Vigor/AppDelegate.swift
+++ Vigor/AppDelegate.swift
@@ -10,17 +10,82 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
+    class var shared: AppDelegate {
+        return NSApplication.shared.delegate as! AppDelegate
+    }
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.variableLength)
+    let status = ""
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        //Menu bar item
+        if let button = statusItem.button {
+            button.title = "VIGOR" + " " + status
+        }
+        //Menu
+        constructMenu()
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        
     }
-
-
+    
+    
 }
 
+extension AppDelegate {
+    func constructMenu() {
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "ID", action: nil, keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: deviceID(), action: nil, keyEquivalent: ""))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Start/Stop", action: #selector(AppDelegate.startStopAnalizing(_:)), keyEquivalent: "s"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Feedback", action: #selector(AppDelegate.sendFeedback(_:)), keyEquivalent: "f"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Exit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
+    }
+    
+    func deviceID() -> String {
+        let theTask = Process()
+        let taskOutput = Pipe()
+        theTask.launchPath = "/sbin/ifconfig"
+        theTask.standardOutput = taskOutput
+        theTask.standardError = taskOutput
+        theTask.arguments = ["en0"]
+        
+        theTask.launch()
+        theTask.waitUntilExit()
+        
+        let taskData = taskOutput.fileHandleForReading.readDataToEndOfFile()
+        
+        if let stringResult = NSString(data: taskData, encoding: String.Encoding.utf8.rawValue) {
+            if stringResult != "ifconfig: interface en0 does not exist" {
+                let f = stringResult.range(of: "ether")
+                if f.location != NSNotFound {
+                    let sub = stringResult.substring(from: f.location + f.length)
+                    let mac = String(sub[sub.index(after: sub.startIndex)..<sub.index(sub.startIndex, offsetBy: 18)])
+                    return mac
+                }
+            }
+        }
+        return "error"
+    }
+    
+}
+
+extension AppDelegate {
+    
+    @objc func startStopAnalizing(_ sender: Any?) {
+        
+    }
+    
+    @objc func sendFeedback(_ sender: Any?) {
+        
+    }
+    
+}
